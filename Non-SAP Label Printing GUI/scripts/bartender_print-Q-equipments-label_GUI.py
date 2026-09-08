6009051343cb3b04b76acb653cff8d8d4fb5cf56
Lâm <20lam.vkt@vinuni.edu.vn>
2026-09-08 10:50:17 +0700

    Modified database directory

--- Non-SAP Label Printing GUI/scripts/bartender_print-Q-equipments-label_GUI.py
+++ Non-SAP Label Printing GUI/scripts/bartender_print-Q-equipments-label_GUI.py
@@ -12,15 +12,20 @@ from pathlib import Path
 
 USER_HOME = os.path.expanduser("~")
 
+# Set root directory relative to RRCVN Label Printing GUI.pyw location
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 SOURCE_FILE = Path(
     USER_HOME + r"\RRC power solutions\RRC VN - Documents\600_Quality\640_Q-Equipments\Q-Equipments-Overview.xlsx"
 )
 
 SOURCE_SHEET = "Q-ID Overview"
 
-OUTPUT_FILE = Path(
-    USER_HOME + r"\RRC power solutions\RRC VN - Documents\999_SHARE_VN\290_IT\Non-SAP Label Printing GUI\databases\Equipment_DB.csv"
-)
+# OUTPUT_FILE = Path(
+#     USER_HOME + r"\RRC power solutions\RRC VN - Documents\999_SHARE_VN\290_IT\Non-SAP Label Printing GUI\databases\Equipment_DB.csv"
+# )
+
+OUTPUT_FILE = BASE_DIR/"databases"/"Equipment_DB.csv"
 
 # ==========================================
 # LOAD EXCEL
